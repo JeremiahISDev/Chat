2c6c8ed52125f688397b3a74a113954a368d681d
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-11-18 22:30:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,33 @@ const projectID = '7ce35b9e-9e14-4bf7-b4e1-b8baf48b659f';
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
+  const showNotification = (message) => {
+    let granted = false;
+		if (Notification.permission === 'granted') {
+			granted = true;
+		} else if (Notification.permission !== 'denied') {
+			let permission = Notification.requestPermission();
+			granted = permission === 'granted' ? true : false;
+		}
+    if (granted) {
+        // create a new notification
+        const notification = new Notification('New Message', {
+          body: `Click To View More or Reply`,
+          icon: 'https://stem-club-chat.netlify.app/favicon.ico'
+        });
+        // close the notification after 10 seconds
+        setTimeout(() => {
+          notification.close();
+        }, 10 * 1000);
+
+        // navigate to a URL when clicked
+        notification.addEventListener('click', () => {
+
+          window.open('https://stem-club-chat.netlify.app/', '_blank');
+        });
+      }
+}
+  
   return (
     <ChatEngine
       height="100vh"
